2d020dc25c8f968a661c4dd1a1eb8789ea8ff4b4
Crazydouzi <2715170004@qq.com>
2023-02-21 14:49:46 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <v-container>
-    <v-row class="mx-auto">
+    <v-row >
       <v-col md="12" sm="12" xs="12" lg="6">
         <v-card>
           <v-card-title>处理器</v-card-title>
           <v-divider></v-divider>
           <v-card-text>
             <!-- 图表 -->
-            <v-progress-circular :rotate="360" :size="200" :width="8" :model-value="cpuInfo.cpuUsage" color="dark"
+            <v-progress-circular :rotate="360" :size="200" :width="8" :model-value="cpuInfo.data.cpuUsage" color="dark"
               class="float-md-left float-lg-left float-xl-left mr-10">
               <div>
                 <br />
-                <p style="font-size:40px;text-align:center">{{ cpuInfo.cpuUsage }}%</p>
+                <p style="font-size:40px;text-align:center">{{ cpuInfo.data.cpuUsage }}%</p>
                 <br />
                 <p> CPU平均使用率</p>
               </div>
@@ -23,13 +23,13 @@
               </v-card-title>
               <v-list flat>
                 <v-list-item>
-                  <v-list-item-title>CPU等待率：{{ cpuInfo.cpuWaitPer }}</v-list-item-title>
+                  <v-list-item-title>CPU等待率：{{ cpuInfo.data.cpuWaitPer }}</v-list-item-title>
                 </v-list-item>
                 <v-list-item>
-                  <v-list-item-title>CPU系统使用率：{{ cpuInfo.cpuSysUsage }}</v-list-item-title>
+                  <v-list-item-title>CPU系统使用率：{{ cpuInfo.data.cpuSysUsage }}</v-list-item-title>
                 </v-list-item>
                 <v-list-item>
-                  <v-list-item-title>CPU用户使用率：{{ cpuInfo.cpuUserUsage }}</v-list-item-title>
+                  <v-list-item-title>CPU用户使用率：{{ cpuInfo.data.cpuUserUsage }}</v-list-item-title>
                 </v-list-item>
               </v-list>
             </v-card>
@@ -42,11 +42,11 @@
           <v-divider></v-divider>
           <v-card-text>
             <!-- 图表 -->
-            <v-progress-circular :rotate="360" :size="200" :width="8" :model-value="memInfo.memUsage" color="dark"
+            <v-progress-circular :rotate="360" :size="200" :width="8" :model-value="memInfo.data.memUsage" color="dark"
               class="float-md-left float-lg-left float-xl-left mr-10">
               <div>
                 <br />
-                <p style="font-size:40px" align="center">{{ memInfo.memUsage }}%</p>
+                <p style="font-size:40px" align="center">{{ memInfo.data.memUsage }}%</p>
                 <br />
                 <p> 内存使用率</p>
 
@@ -55,17 +55,17 @@
             <!-- 信息 -->
             <v-card flat>
               <v-card-title class="headline font-weight-bold">
-                内存占比： {{ memInfo.memFree + " / " + memInfo.memTotal }}
+                占比： {{ memInfo.data.memFree + " / " + memInfo.data.memTotal }}
               </v-card-title>
               <v-list flat>
                 <v-list-item>
-                  <v-list-item-title>空闲内存：{{ memInfo.memFree }}</v-list-item-title>
+                  <v-list-item-title>空闲内存：{{ memInfo.data.memFree }}</v-list-item-title>
                 </v-list-item>
                 <v-list-item>
-                  <v-list-item-title>内存总量：{{ memInfo.memTotal }}</v-list-item-title>
+                  <v-list-item-title>内存总量：{{ memInfo.data.memTotal }}</v-list-item-title>
                 </v-list-item>
                 <v-list-item>
-                  <v-list-item-title>JVM占用量：{{ memInfo.memTotal }}</v-list-item-title>
+                  <v-list-item-title>JVM占用量：{{ memInfo.data.jvmMemUse }}MB</v-list-item-title>
                 </v-list-item>
               </v-list>
             </v-card>
@@ -74,13 +74,44 @@
 
       </v-col>
     </v-row>
+    <v-spacer></v-spacer>
+    <v-card class="mt-5">
+      <v-card-title>系统信息</v-card-title>
+      <v-divider></v-divider>
+    </v-card>
   </v-container>
 </template>
 
 <script lang="ts" setup>
 import { ref, reactive } from 'vue';
-const memInfo = reactive({ memUsage: 61, memTotal: "23.1G", memUse: "8G", memFree: "8G" })
-const cpuInfo = reactive({ cpuUsage: 2, cpuWaitPer: "0%", cpuSysUsage: "0.35%", cpuUserUsage: "1.29%" })
+import axios from 'axios';
+interface MemoryInterface{
+  memTotal:string;
+  memFree:string;
+  memUsage:number;
+  memUse:string;
+  jvmMemUse:number;
+}
+let cpuCoreCount="8核心/16线程";
+let memInfo = reactive({data:{ memUsage: 0, memTotal: "0", memUse: "0", memFree: "" ,jvmMemUse:0}})
+let cpuInfo = reactive({data:{ cpuUsage: 0, cpuWaitPer: "0%", cpuSysUsage: "", cpuUserUsage: "" }})
+const headers:object={
+  'Content-Type':'application/json'
+}
+setInterval(()=>{
+  getUsageInfo();
+},7000)
+
+function getUsageInfo(){
+  axios.post("http://localhost:8080/api/system/getCpuUsage",headers).then(r=>{
+    cpuInfo.data=r.data.data
+})
+  axios.post("http://localhost:8080/api/system/getMemUsage",headers).then(r=>{
+    let data:MemoryInterface=r.data.data
+    memInfo.data=data
+  })
+}
+function getSystemInfo(){}
 </script>
 <style>
 
