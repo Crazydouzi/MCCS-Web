bc8b7514c577d84857d79ce7c26990b579619a5f
Crazydouzi <2715170004@qq.com>
2023-03-27 00:23:22 +0800

    实例上传功能

--- src/components/AddInstance.vue
+++ src/components/AddInstance.vue
@@ -1,39 +1,59 @@
 <template>
-  <v-form>
+  <v-form @submit.prevent="save()" fast-fail>
     <v-container class="fill-height" fluid>
       <v-list style="width: 100%;">
         <h3 class="my-3">基本配置</h3>
         <v-list-item>
           <v-list-item-title>服务器名</v-list-item-title>
-          <v-text-field :counter="10" label="服务器名" required variant="underlined"></v-text-field>
+          <v-text-field :counter="10" label="服务器名" required variant="underlined" :rules="[rules.required]"
+            v-model="MCServer.serverName"></v-text-field>
         </v-list-item>
-
+        <h3 class="my-3">版本配置</h3>
         <v-list-item>
+          <v-list-item-title>请选择CORE包来源</v-list-item-title>
+          <v-select :items="['paper', '用户上传']" v-model="versionInfo.from" change="alert" variant="underlined"
+            :rules="[rules.required]"></v-select>
+        </v-list-item>
+        <v-list-item v-if="versionInfo.from == 'paper'">
           <v-list-item-title>请选择版本</v-list-item-title>
-          <v-select :items="versionList" v-model="selectVersion" change="alert" variant="underlined"></v-select>
+          <v-select :items="versionList" v-model="selectVersion" change="alert" variant="underlined"
+            :rules="[rules.required]"></v-select>
+        </v-list-item>
+        <v-list-item v-else-if="versionInfo.from === '用户上传'">
+          <v-row>
+            <v-col>
+              <v-file-input label="请选择Core" required variant="underlined" :rules="[rules.required]"
+                accept=".jar"></v-file-input>
+            </v-col>
+            <v-col><v-text-field label="版本" required variant="underlined"
+                :rules="[rules.required]"></v-text-field></v-col>
+          </v-row>
         </v-list-item>
         <v-divider color="black"></v-divider>
         <h3 class="my-3">参数配置</h3>
         <v-list-item>
           <v-list-item-title>Java路径</v-list-item-title>
-          <v-text-field :counter="10" label="JavaVersion" required variant="underlined"></v-text-field>
+          <v-text-field :counter="10" label="JavaVersion" required variant="underlined" v-model="MCSetting.javaVersion"
+            :rules="[rules.required]"></v-text-field>
         </v-list-item>
         <v-list-item>
           <v-list-item-title>内存分配</v-list-item-title>
           <v-row>
-            <v-col><v-text-field label="最大值" required variant="underlined"></v-text-field></v-col>
-            <v-col><v-text-field label="最小值" required variant="underlined"></v-text-field></v-col>
+            <v-col><v-text-field label="最小值" required variant="underlined" v-model="MCSetting.memMin"
+                :rules="[rules.required]"></v-text-field></v-col>
+            <v-col><v-text-field label="最大值" required variant="underlined" v-model="MCSetting.memMax"
+                :rules="[rules.required]"></v-text-field></v-col>
           </v-row>
         </v-list-item>
         <v-list-item>
           <v-list-item-title>启动参数设置</v-list-item-title>
-          <v-text-field :counter="10" label="VMOptions" required variant="underlined"></v-text-field>
+          <v-text-field :counter="10" label="VMOptions" variant="underlined" v-model="MCSetting.VMOptions"></v-text-field>
         </v-list-item>
         <v-list-item>
           <v-row>
             <v-col>
               <v-spacer></v-spacer>
-              <v-btn color="blue" class="float-right" @click="getDownLoadLink()">保存</v-btn>
+              <v-btn color="blue" class="float-right" type="submit">保存</v-btn>
             </v-col>
             <v-col>
               <v-btn color="red" @click="close">取消</v-btn>
@@ -49,8 +69,38 @@
 </template>
 
 <script lang="ts" setup>
-import { defineEmits, onBeforeMount, ref } from 'vue'
+import {  onBeforeMount, reactive, ref } from 'vue'
 import $API from '@/core/api/fetch';
+interface MCSetting {
+  javaVersion: String,
+  memMin: String,
+  memMax: String,
+  VMOptions: String,
+  jarName: String
+
+}
+interface MCServer {
+  serverName: String,
+  version: String,
+
+}
+let MCSetting = reactive<MCSetting>({
+  javaVersion: "java",
+  memMin: "1G",
+  memMax: "2G",
+  VMOptions: undefined,
+  jarName: undefined
+})
+let MCServer = reactive<MCServer>({
+  serverName: undefined,
+  version: undefined
+})
+let versionInfo = reactive({
+  from: "paper",
+  version: "",
+  buildCode: "",
+  coreName: ""
+})
 let versionList = ref([])
 let selectVersion = ref()
 let request = {
@@ -60,33 +110,48 @@ let request = {
     accept: "application/json"
   }
 }
+const rules: any = {
+  required: (value: any) => value ? true : '此项不能为空',
+}
 const emit = defineEmits(['close'])
 function close() {
   emit('close')
 }
 function getPaperList() {
-  const paperRequest = request
+  const paperRequest = Object.assign({}, request)
   $API.request(paperRequest).then(r => {
-    versionList.value = r.version_groups
-    selectVersion.value = versionList.value.pop()
+    versionList.value = r.versions
+    console.log(versionList.value)
+    selectVersion.value = versionList.value.at(-1)
   })
 }
-function getDownLoadLink() {
-  let build = request
-  let buildCode = "";
-  let coreName = ""
+async function getDownLoadLink() {
+  versionInfo.version = selectVersion.value
+  let build = Object.assign({}, request)
   build.url = build.url + "/versions/" + selectVersion.value
   $API.request(build).then(r => {
-    buildCode = r.builds.pop()
+    versionInfo.buildCode = r.builds.pop()
   }).then(async () => {
-    build.url=build.url+"/builds/"+buildCode
+    build.url = build.url + "/builds/" + versionInfo.buildCode
+    console.log(build.url)
     await $API.request(build).then(r => {
-      coreName=r.downloads.application.name
-      console.log("url:"+build.url+"/downloads/"+coreName)
+      versionInfo.coreName = r.downloads.application.name
     })
-  }).then(()=>{
-    console.log("完成")
   })
+}
+function save() {
+  if (versionInfo.from != "用户上传") { } else {
+    getDownLoadLink().then(() => {
+      console.log("完成")
+      MCServer.version = selectVersion.value
+      let data = {
+        MCServer,
+        MCSetting,
+        versionInfo
+      }
+      console.log(data)
+    })
+  }
 
 }
 onBeforeMount(() => {
--- src/components/UploadInstance.vue
+++ src/components/UploadInstance.vue
@@ -1,79 +1,118 @@
 <template>
-  <v-container class="fill-height">
-    <v-responsive class="d-flex align-center text-center fill-height">
-      <v-img
-        contain
-        height="300"
-        src="@/assets/logo.svg"
-      />
+  <v-form @submit.prevent="save()" fast-fail>
+    <v-container class="fill-height" fluid>
+      <v-list style="width: 100%;">
+        <h3 class="my-3">基本配置</h3>
+        <v-list-item>
+          <v-list-item-title>服务器名</v-list-item-title>
+          <v-text-field :counter="10" label="服务器名" required variant="underlined" :rules="[rules.required]"
+            v-model="MCServer.serverName"></v-text-field>
+        </v-list-item>
+        <h3 class="my-3">整合包文件</h3>
+        <v-list-item>
+          <v-row>
+            <v-col>
+              <v-file-input label="请选择整合包" required variant="underlined" :rules="[rules.required]"
+                accept=".zip"></v-file-input>
+            </v-col>
+            <v-col><v-text-field label="版本" required variant="underlined"
+                :rules="[rules.required]"></v-text-field></v-col>
+          </v-row>
+        </v-list-item>
+        <v-divider color="black"></v-divider>
+        <h3 class="my-3">参数配置</h3>
+        <v-list-item>
+          <v-select :items="[{ title: '自动扫描', value: 'true' }, { title: '手动配置', value: false }]" variant="underlined"
+            v-model="autoScan" :hint="autoScan ? '请确保整合包文件包含.sh或.bat启动文件' : ''" persistent-hint class="select-box"></v-select>
+        </v-list-item>
+        <div v-show="!autoScan">
+          <v-list-item>
+            <v-list-item-title>Java路径</v-list-item-title>
+            <v-text-field :counter="10" label="JavaVersion" required variant="underlined" v-model="MCSetting.javaVersion"
+              :rules="[rules.required]"></v-text-field>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>内存分配</v-list-item-title>
+            <v-row>
+              <v-col><v-text-field label="最小值" required variant="underlined" v-model="MCSetting.memMin"
+                  :rules="[rules.required]"></v-text-field></v-col>
+              <v-col><v-text-field label="最大值" required variant="underlined" v-model="MCSetting.memMax"
+                  :rules="[rules.required]"></v-text-field></v-col>
+            </v-row>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>启动参数设置</v-list-item-title>
+            <v-text-field :counter="10" label="VMOptions" variant="underlined"
+              v-model="MCSetting.VMOptions"></v-text-field>
+          </v-list-item>
+        </div>
 
-      <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+        <v-list-item>
+          <v-row>
+            <v-col>
+              <v-spacer></v-spacer>
+              <v-btn color="blue" class="float-right" type="submit">保存</v-btn>
+            </v-col>
+            <v-col>
+              <v-btn color="red" @click="close">取消</v-btn>
+              <v-spacer></v-spacer>
+            </v-col>
+          </v-row>
+        </v-list-item>
+      </v-list>
 
-      <h1 class="text-h2 font-weight-bold">Vuetify</h1>
+    </v-container>
 
-      <div class="py-14" />
-
-      <v-row class="d-flex align-center justify-center">
-        <v-col cols="auto">
-          <v-btn
-            href="https://next.vuetifyjs.com/components/all/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
-            <v-icon
-              icon="mdi-view-dashboard"
-              size="large"
-              start
-            />
-
-            Components
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            color="primary"
-            href="https://next.vuetifyjs.com/introduction/why-vuetify/#feature-guides"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            target="_blank"
-            variant="flat"
-          >
-            <v-icon
-              icon="mdi-speedometer"
-              size="large"
-              start
-            />
-
-            Get Started
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            href="https://community.vuetifyjs.com/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
-            <v-icon
-              icon="mdi-account-group"
-              size="large"
-              start
-            />
-
-            Community
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-responsive>
-  </v-container>
+  </v-form>
 </template>
 
 <script lang="ts" setup>
-  //
+import { onBeforeMount, reactive, ref } from 'vue'
+import $API from '@/core/api/fetch';
+// import { title } from 'process';
+interface MCSetting {
+  javaVersion: String,
+  memMin: String,
+  memMax: String,
+  VMOptions: String,
+  jarName: String
+}
+interface MCServer {
+  serverName: String,
+  version: String,
+
+}
+let MCSetting = reactive<MCSetting>({
+  javaVersion: "java",
+  memMin: "1G",
+  memMax: "2G",
+  VMOptions: undefined,
+  jarName: undefined
+})
+let MCServer = reactive<MCServer>({
+  serverName: undefined,
+  version: undefined
+})
+let autoScan = ref({ title: '自动扫描', value: true })
+const rules: any = {
+  required: (value: any) => value ? true : '此项不能为空',
+}
+const emit = defineEmits(['close'])
+function close() {
+  emit('close')
+}
+
+
+function save() {
+
+}
+onBeforeMount(() => {
+})
 </script>
+<style>
+.select-box .v-messages__message {
+  color: red;
+  font-weight: bold;
+  font-size: small;
+}
+</style>
