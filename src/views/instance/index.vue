bfef1c2f2a8947e97457b38a53a1c48b0ac7b345
Crazydouzi <2715170004@qq.com>
2023-03-27 00:23:07 +0800

    增加路由切换动画效果

--- src/views/instance/index.vue
+++ src/views/instance/index.vue
@@ -1,10 +1,11 @@
 <template>
-  <v-main mini-variant.sync>
+  <v-container :fluid=true>
     <router-view style="background-color:#f3f3f3;" v-slot="{ Component,route }">
       <v-scale-transition origin="top left" :leave-absolute=true>
-          <component :is="Component" :key="route.path"></component>
+          <component :is="Component" ><div></div></component>
       </v-scale-transition>
     </router-view>
-  </v-main>
-</template>
 
+  </v-container>
+</template>
+<script lang="ts" setup></script>
