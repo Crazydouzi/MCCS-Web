993f495dc8bbcd1eedec7b690ebe32f5509b2715
Crazydouzi <2715170004@qq.com>
2023-04-01 00:34:12 +0800

    添加修改配置文件页面

---
+++ src/components/serverConfig/ConfigEdit.vue
@@ -0,0 +1,23 @@
+<template>
+  <component :is="page"></component>
+</template>
+<script setup lang="ts">
+import { shallowRef } from 'vue';
+let page = shallowRef(null)
+let props = defineProps(["config"])
+switch (props.config) {
+  case "bukkit":
+    page = () => import('./Bukkit.vue')
+    break;
+  case "spigot":
+    page = () => import('./Spigot.vue')
+    break;
+  case "properties":
+    page = () => import('./Properties.vue')
+    break;
+  default:
+    page = () => import('./Properties.vue')
+    break;
+}
+</script>
+
